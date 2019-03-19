af87909729361a842cb112c065e5c46fdb6a72ee
xzlyf <czr2001@outlook.com>
2019-03-19 13:05:32 +0800

    增加网络变化侦听广播

---
+++ app/src/main/java/com/xz/bing/MyApplication.java
@@ -0,0 +1,6 @@
+package com.xz.bing;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+}
---
+++ app/src/main/java/com/xz/bing/broadcast/NetworkChangeReceiver.java
@@ -0,0 +1,6 @@
+package com.xz.bing.broadcast;
+
+import android.content.BroadcastReceiver;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+}
